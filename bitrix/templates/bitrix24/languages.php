b7bccec5e6bfd56a39dc333169f91c4a5c247e4e
rodnie1 <rodnietg@gmail.com>
2018-05-17 18:32:10 +0800

    5
    5

---
+++ bitrix/templates/bitrix24/languages.php
@@ -0,0 +1,17 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+// duplicate to templates/pub
+$b24Languages = array(
+	"ru" => "Русский",
+	"en" => "English",
+	"de" => "Deutsch",
+	"ua" => "Українська",
+	"la" => "Español",
+	"tc" => "中文（繁體）",
+	"sc" => "中文（简体）",
+	"br" => "Português (BR)",
+	"in" => "Indian",
+	"fr" => "Français, beta",
+	"pl" => "Polski, beta",
+	"tr" => "Türkçe, beta"
+);
+?>
